216416f7d5406abd6f3c383c7000a8225bd0788c
niltonnluiz-tech <niltonnluiz@gmail.com>
2026-08-03 10:19:28 -0300

    Delete qsmaestro (13) directory

--- qsmaestro (13)/MAESTTRO/codigo-completo-multiarquivo/supabase-config.js
+++
@@ -1,6 +0,0 @@
-// MAESTTRO - Configuração do Supabase
-window.MAESTTRO_SUPABASE = {
-  url: "https://zbkhhhahrnprrqtzuhhq.supabase.co",
-  anonKey: "sb_publishable_bSqiOiWv0AgjoowWi9A8BQ_YY7k3RIq"
-};
-
